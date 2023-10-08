ad14e1976a3c064bcedaf327001c73aa7c515dc8
materus <materus@podkos.pl>
2023-10-08 16:14:06 +0200

    inputs

--- configurations/profile/common/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, materusFlake, inputs, ... }:
-{
-  imports = [
-    ./nixpkgs.nix
-    ./packages
-  ];
-  config._module.args.materusPkgs = (import inputs.configInputs.inputs.nixerus { inherit pkgs; }) //
-  (if pkgs.system == "x86_64-linux" then { i686Linux = import inputs.configInputs.inputs.nixerus { pkgs = pkgs.pkgsi686Linux; }; } else { });
-}
