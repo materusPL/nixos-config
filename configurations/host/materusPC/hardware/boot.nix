154d850e49f59806a94613aba0af1052420f4b3d
materus <materus@podkos.pl>
2023-10-27 12:30:38 +0200

    configurations: formatting

--- configurations/host/materusPC/hardware/boot.nix
+++ configurations/host/materusPC/hardware/boot.nix
@@ -1,41 +1,41 @@
 { config, pkgs, lib, inputs, materusFlake, ... }:
-let 
-video = [
-  
-  "video=HDMI-A-3:1920x1080@144"
-  "video=DP-3:1920x1080@240"
-  
+let
+  video = [
 
-  #"video=DP-1:1920x1080@240"
-  #"video=DP-2:1920x1080@240"
-  #"video=HDMI-A-1:1920x1080@240"
-  #"video=HDMI-A-2:1920x1080@240"
-  
+    "video=HDMI-A-3:1920x1080@144"
+    "video=DP-3:1920x1080@240"
 
-];
-in 
+
+    #"video=DP-1:1920x1080@240"
+    #"video=DP-2:1920x1080@240"
+    #"video=HDMI-A-1:1920x1080@240"
+    #"video=HDMI-A-2:1920x1080@240"
+
+
+  ];
+in
 {
   #Kernel
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.kernelParams = [ "pcie_acs_override=downstream,multifunction" /*"pci-stub.ids=1002:744c"*/ "nox2apic" "nvme_core.default_ps_max_latency_us=0" "nvme_core.io_timeout=255" "nvme_core.max_retries=10" "nvme_core.shutdown_timeout=10" "amd_iommu=on" "iommu=pt"] ++ video;
+  boot.kernelParams = [ "pcie_acs_override=downstream,multifunction" /*"pci-stub.ids=1002:744c"*/ "nox2apic" "nvme_core.default_ps_max_latency_us=0" "nvme_core.io_timeout=255" "nvme_core.max_retries=10" "nvme_core.shutdown_timeout=10" "amd_iommu=on" "iommu=pt" ] ++ video;
   boot.kernelModules = [ "pci-stub" "amdgpu" "i2c_dev" "kvm_amd" "vfio" "vfio_iommu_type1" "vfio-pci" "v4l2loopback" ];
   boot.extraModprobeConfig = ''
-  options kvm_amd nested=1 avic=1 npt=1
-  options vfio_iommu_type1 allow_unsafe_interrupts=1
+    options kvm_amd nested=1 avic=1 npt=1
+    options vfio_iommu_type1 allow_unsafe_interrupts=1
   '';
   boot.kernel.sysctl = {
-                        "vm.max_map_count" = 1000000;
-                        "vm.swappiness" = 10;
-                       };
-  
-  
+    "vm.max_map_count" = 1000000;
+    "vm.swappiness" = 10;
+  };
+
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  
-  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
-  
 
-  boot.supportedFilesystems = [ "ntfs" "btrfs" "vfat" "exfat" "ext4"];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+
+
+  boot.supportedFilesystems = [ "ntfs" "btrfs" "vfat" "exfat" "ext4" ];
 
   boot.tmp.useTmpfs = true;
 
@@ -52,4 +52,6 @@ in
     useOSProber = true;
     memtest86.enable = true;
   };
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 }
