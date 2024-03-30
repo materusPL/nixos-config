c8d584f8a4c0899cf843f55b1a398c00f7ce08bf
materus <materus@podkos.pl>
2024-03-30 15:41:52 +0100

    configuration: prepare for browser changes

--- configurations/profile/home/browser.nix
+++ configurations/profile/home/browser.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, materusArg, ... }:
 let
   cfg = config.materus.profile.browser;
+  osConfig = (if (builtins.hasAttr "osConfig" config._module.args) then config._module.args.osConfig else null);
 in
 {
 
@@ -11,13 +12,14 @@ in
 
   };
   #TODO: Make some config
-  config = {
+  config = lib.mkMerge [{
       home.packages = [
       (lib.mkIf cfg.firefox.enable config.materus.profile.packages.firefox)
       (lib.mkIf cfg.vivaldi.enable pkgs.vivaldi)
       (lib.mkIf cfg.brave.enable pkgs.brave)
     ];
-  };
+  }
+  ];
 
 
 }
