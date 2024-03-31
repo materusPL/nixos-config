a1eccd4d7795a0c997d3d62f1b49c3feb269aa2f
materus <materus@podkos.pl>
2024-03-31 22:28:57 +0200

    home-profile: move wezterm config to own file

--- configurations/profile/home/terminal/wezterm.nix
+++ configurations/profile/home/terminal/wezterm.nix
@@ -1,17 +1,12 @@
 { config, lib, materusArg, pkgs, ... }:
 let
   cfg = config.materus.profile.wezterm;
-  cfgText = ''
-    config.hide_tab_bar_if_only_one_tab = true;
-    config.enable_scroll_bar = true;
-    config.font = wezterm.font 'Hack Nerd Font';
-  '';
 in
 {
   options.materus.profile.wezterm.enable = materusArg.pkgs.lib.mkBoolOpt config.materus.profile.enableDesktop "Enable materus wezterm config";
   options.materus.profile.wezterm.enableHackFont = materusArg.pkgs.lib.mkBoolOpt true "Enable hack nerd font for wezterm";
-  options.materus.profile.wezterm.config = lib.mkOption {
-    default = cfgText;
+  options.materus.profile.wezterm.extraConfig = lib.mkOption {
+    default = "";
     description = "Config for wezterm";
     type = lib.types.string;
   };
@@ -19,9 +14,11 @@ in
     programs.wezterm.enable = true;
     programs.wezterm.colorSchemes = { };
     programs.wezterm.extraConfig = ''
-      local config = wezterm.config_builder();
 
-      ${cfgText}
+      package.path = package.path .. ";${materusArg.cfg.path}/extraFiles/config/wezterm/?.lua"
+      require("wezterm_config");
+      local config = materus_wezterm_config();
+      ${cfg.extraConfig}
 
       return config;
     '';
