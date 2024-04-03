6b5b53956b18c2353c01d50538c1e651ce9dcb4a
materus <materus@podkos.pl>
2024-04-03 10:52:20 +0200

    waffentrager: make auth emtpy

--- configurations/host/waffentrager/services/auth.nix
+++ configurations/host/waffentrager/services/auth.nix
@@ -10,18 +10,5 @@
       waffentragerService.elements.enable = true;
       waffentragerService.nginx.enable = true;
 
-      services.postgresql.enable = true;
-      services.postgresql.package = pkgs."postgresql_${cfg.version}";
-      services.postgresql.dataDir = "${config.waffentragerService.elements.postgresqlDir}/${cfg.version}";
-      services.postgresql.enableJIT = true;
-      services.postgresql.authentication = pkgs.lib.mkOverride 10 ''
-        local all all trust
-        host all all 127.0.0.1/32 scram-sha-256
-      '';
-      systemd.services.postgresql = {
-        partOf = [ "elements-mount.service" ];
-        requires = [ "elements-mount.service" ];
-        after = [ "elements-mount.service" ];
-      };
     };
 }
