4cd531103acc2dcad55bae88ead6c1d8c23b6d2d
materus <materus@podkos.pl>
2024-04-06 15:10:57 +0200

    os-config: set zsh as default shell for users

--- configurations/profile/os/shell/zsh.nix
+++ configurations/profile/os/shell/zsh.nix
@@ -5,10 +5,15 @@ in
 {
   options.materus.profile.zsh.enable = materusArg.pkgs.lib.mkBoolOpt true "Enable materus system zsh config";
   config =  lib.mkIf cfg.enable {
+    users.defaultUserShell = pkgs.zsh;
     programs.zsh = {
       enable = true;
       enableGlobalCompInit=false;
       interactiveShellInit = ''
+        if [[ $(${pkgs.coreutils}/bin/echo $TTY | ${pkgs.gnugrep}/bin/grep "/dev/tty") ]] && [[ "$TERM" == "linux"  ]]; then
+           setfont lat2-16 -m 8859-2
+        fi
+
         if [[ ''${__MATERUS_HM_ZSH:-0} == 0 ]]; then
           source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
         fi
