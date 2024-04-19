2959e10532733143de1c86c27c6aa17bd1a171c7
materus <materus@podkos.pl>
2024-04-19 17:21:22 +0200

    waffentrager: remove cert

--- configurations/host/waffentrager/secrets/default.nix
+++ configurations/host/waffentrager/secrets/default.nix
@@ -23,7 +23,6 @@
   ];
   sops.secrets.wireguard = { };
   sops.secrets."users/materus" = { neededForUsers = true; };
-  sops.secrets.certs = { };
   sops.secrets.elements = { };
   sops.secrets.nextcloud-adminpass = { };
 }
