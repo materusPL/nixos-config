c4beb9542b45b33da78d8a7af71241baa0f2138c
materus <materus@podkos.pl>
2024-05-02 20:20:37 +0200

    Old-materusPC: portal settings

--- configurations/host/Old-materusPC/plasma.nix
+++ configurations/host/Old-materusPC/plasma.nix
@@ -16,6 +16,10 @@
     QT_PLUGIN_PATH = [ "${pkgs.qt6.qtimageformats}/${pkgs.qt6.qtbase.qtPluginPrefix}" ];
     XCURSOR_THEME = "breeze_cursors";
   };
+  xdg.portal.enable = true;
+  xdg.portal.wlr.enable = true;
+  xdg.portal.xdgOpenUsePortal = true;
+  xdg.portal.extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
   services.desktopManager.plasma6.enable = true;
   services.desktopManager.plasma6.enableQt5Integration = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [ kwallet kwalletmanager kwallet-pam ];
