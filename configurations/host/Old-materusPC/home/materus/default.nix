636eb2a8e628bd57b743eaab8201ea2fa29f6981
materus <materus@podkos.pl>
2024-06-16 13:23:48 +0200

    flake: update lock, Old-materusPC: brave settings

--- configurations/host/Old-materusPC/home/materus/default.nix
+++ configurations/host/Old-materusPC/home/materus/default.nix
@@ -72,7 +72,6 @@
 
   };
   xdg.desktopEntries.brave-browser = let env = lib.concatStringsSep " " [
-    ''VK_ICD_FILENAMES=''$VK_ICD_FILENAMES:/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json:/run/opengl-driver-32/share/vulkan/icd.d/nvidia_icd.i686.json''
     ''__NV_PRIME_RENDER_OFFLOAD="1"''
     ''__NV_PRIME_RENDER_OFFLOAD_PROVIDER="NVIDIA-G0"''
     ''__GLX_VENDOR_LIBRARY_NAME="nvidia"''
