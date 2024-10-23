1056568b6c79dff9f6a961ea13ba76e1cc113b00
materus <materus@podkos.pl>
2024-10-23 15:28:13 +0200

    materusPC: plasma-manager disable kwallet

---
+++ configurations/host/materusPC/home/materus/plasma.nix
@@ -0,0 +1,124 @@
+{ pkgs, ... }:
+{
+  home.packages = [
+    pkgs.papirus-icon-theme
+    (pkgs.nerdfonts.override { fonts = [ "Hack" ]; })
+  ];
+  programs.konsole = {
+    enable = true;
+    profiles = {
+      materus = {
+        colorScheme = "Breeze";
+        font.name = "Hack Nerd Font";
+        extraConfig = {
+          Scrolling = {
+            HistoryMode = 2;
+          };
+        };
+      };
+    };
+    extraConfig = {
+      KonsoleWindow = {
+        RememberWindowSize = "false";
+      };
+      MainWindow = {
+        MenuBar = "Enabled";
+      };
+
+      "Desktop Entry" = {
+        DefaultProfile = "materus.profile";
+      };
+    };
+  };
+  programs.plasma = {
+    enable = true;
+    overrideConfig = false;
+
+
+
+    workspace = {
+      lookAndFeel = "org.kde.breezedark.desktop";
+      iconTheme = "Papirus-Dark";
+    };
+
+    shortcuts = {
+      "kwin"."Grid View" = "Meta+Alt+Tab";
+      "kwin"."Overview" = "Meta+Tab";
+    };
+    spectacle.shortcuts = {
+      captureActiveWindow = "Meta+Print";
+      captureCurrentMonitor = "Print";
+      captureEntireDesktop = "Shift+Print";
+      captureRectangularRegion = "Meta+S";
+      launchWithoutCapturing = "Meta+Shift+S";
+      launch = "Meta+Alt+S";
+    };
+
+    kwin = {
+      effects = {
+        wobblyWindows.enable = true;
+      };
+    };
+
+    input = {
+      keyboard = {
+        options = [ "caps:none" ];
+      };
+    };
+    kscreenlocker = {
+      autoLock = false;
+    };
+
+    panels = [
+      {
+        location = "left";
+        screen = 0;
+        widgets = [
+          {
+            name = "org.kde.plasma.kickerdash";
+            config = {
+              General = {
+                icon = "nix-snowflake-white";
+                alphaSort = true;
+              };
+            };
+
+          }
+          "org.kde.plasma.icontasks"
+          "org.kde.plasma.marginsseparator"
+          "org.kde.plasma.systemtray"
+          {
+            name = "org.kde.plasma.digitalclock";
+            config = {
+              Appearance = {
+                showDate = "false";
+              };
+            };
+          }
+        ];
+
+      }
+
+    ];
+    configFile = {
+      "kwinrc"."Effect-overview"."BorderActivate" = 9;
+
+      "kcminputrc"."Libinput/9610/46/SINOWEALTH Wired Gaming Mouse"."PointerAccelerationProfile" = 1;
+      "kcminputrc"."Libinput/9610/47/SINOWEALTH 2.4G Wireless Receiver"."PointerAccelerationProfile" = 1;
+
+      "spectaclerc"."ImageSave"."imageFilenameTemplate" = "<yyyy>-<MM>-<dd>.<hh><mm><ss>-<t>.materusPC.<#>";
+      "spectaclerc"."ImageSave"."preferredImageFormat" = "WEBP";
+      "spectaclerc"."ImageSave"."translatedScreenshotsFolder" = "Zrzuty ekranu";
+      "spectaclerc"."VideoSave"."translatedScreencastsFolder" = "Nagranie ekranu";
+
+      "dolphinrc"."General"."RememberOpenedTabs" = false;
+
+      "kwalletrc"."Wallet"."Enabled" = false;
+
+    };
+    dataFile = {
+      "dolphin/view_properties/global/.directory"."Settings"."HiddenFilesShown" = true;
+    };
+
+  };
+}
