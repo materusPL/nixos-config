0a20666db2f155896b5e0dd2d3ef38fe02553953
materus <materus@podkos.pl>
2024-11-10 13:00:18 +0100

    configurations: emacs tests

--- extraFiles/config/emacs/early-init.el
+++ extraFiles/config/emacs/early-init.el
@@ -13,8 +13,8 @@
 (unless (daemonp)
   (setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
       gc-cons-percentage 0.6)
-  
-  
+
+
   (advice-add #'tty-run-terminal-initialization :override #'ignore)
   (add-hook 'window-setup-hook
     (lambda ()
